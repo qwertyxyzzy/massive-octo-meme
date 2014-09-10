9a0b642e9d6ea3366dfe729b3c8fe94b64e4a5d2
tim <git@tpm.homeip.net>
2014-09-10 21:47:53 +0100

    new rep import

---
+++ src/com/ka/spreadsheet/diff/SpreadSheetExcel.java
@@ -0,0 +1,173 @@
+package com.ka.spreadsheet.diff;
+
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class SpreadSheetExcel implements ISpreadSheet {
+
+	private Workbook workbook;
+	
+	public SpreadSheetExcel(Workbook workbook) {
+		this.workbook = workbook;
+	}
+	
+	@Override
+	public Iterator<ISheet> getSheetIterator() {
+		return new Iterator<ISheet>() {
+			
+			private int currSheetIdx = 0;
+
+			@Override
+			public boolean hasNext() {
+				return currSheetIdx < workbook.getNumberOfSheets();
+			}
+
+			@Override
+			public ISheet next() {
+				Sheet sheet = workbook.getSheetAt(currSheetIdx);
+				SheetExcel sheetExcel = new SheetExcel(sheet, currSheetIdx);
+				currSheetIdx++;
+				return sheetExcel;
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+}
+
+class SheetExcel implements ISheet {
+
+	private Sheet sheet;
+	private int sheetIdx;
+	
+	public SheetExcel(Sheet sheet, int sheetIdx) {
+		this.sheet = sheet;
+		this.sheetIdx = sheetIdx;
+	}
+	
+	@Override
+	public String getName() {
+		return sheet.getSheetName();
+	}
+	
+	@Override
+	public int getSheetIndex() {
+		return sheetIdx;
+	}
+
+	@Override
+	public Iterator<IRow> getRowIterator() {
+		final Iterator<Row> rowIterator = sheet.rowIterator();
+		return new Iterator<IRow>() {
+			
+			@Override
+			public boolean hasNext() {
+				return rowIterator.hasNext();
+			}
+
+			@Override
+			public IRow next() {
+				return new RowExcel(rowIterator.next());
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+}
+
+class RowExcel implements IRow {
+	
+	private Row row;
+	
+	public RowExcel(Row row) {
+		this.row = row;
+	}
+
+	@Override
+	public int getRowIndex() {
+		return row.getRowNum();
+	}
+
+	@Override
+	public Iterator<ICell> getCellIterator() {
+		final Iterator<Cell> cellIterator = row.cellIterator();
+		return new Iterator<ICell>() {
+			
+			@Override
+			public boolean hasNext() {
+				return cellIterator.hasNext();
+			}
+
+			@Override
+			public ICell next() {
+				return new CellExcel(cellIterator.next());
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+}
+
+class CellExcel implements ICell {
+
+	private Cell cell;
+	
+	public CellExcel(Cell cell) {
+		this.cell = cell;
+	}
+
+	@Override
+	public int getRowIndex() {
+		return cell.getRowIndex();
+	}
+
+	@Override
+	public int getColumnIndex() {
+		return cell.getColumnIndex();
+	}
+
+    // we use this function to display the cells that we already know are
+    // different. For formulae we display the cached value as that is useful
+    // we it comes from a linked sheet that we can not see
+	@Override
+	public String getDisplayValue() {
+		int cellType = cell.getCellType();
+		switch (cellType) {
+		case Cell.CELL_TYPE_FORMULA:
+		    return String.format("%s = %s", String.valueOf(cell.getNumericCellValue()), getStringValue());
+		}
+		return getStringValue();
+	}
+
+	@Override
+	public String getStringValue() {
+		int cellType = cell.getCellType();
+		switch (cellType) {
+		case Cell.CELL_TYPE_NUMERIC:
+			return String.valueOf(cell.getNumericCellValue());
+		case Cell.CELL_TYPE_BOOLEAN:
+			return String.valueOf(cell.getBooleanCellValue());
+		case Cell.CELL_TYPE_BLANK:
+		case Cell.CELL_TYPE_STRING:
+			return cell.getStringCellValue();
+		case Cell.CELL_TYPE_FORMULA:
+			return cell.getCellFormula();
+		case Cell.CELL_TYPE_ERROR:
+			return String.valueOf(cell.getErrorCellValue());
+		}
+		return cell.getStringCellValue();
+	}
+}
